d47c01e777d2367a6891c83363781dd4c1243beb
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-01-05 04:01:51 +0300

    add entity destruction

--- src/entity.c
+++ src/entity.c
@@ -4,14 +4,126 @@
 #include <stdlib.h>
 
 #include "saveload.h"
+#include "entitymap.h"
+#include "pool.h"
 
-static unsigned int top = 0;
+static int dummy;
+static void *dummy_ptr = &dummy; /* put in EntityMap to differentiate from
+                                    NULL */
+
+static EntityMap *emap;
+
+static EntityMap *destroyed_map; /* whether entity is destroyed */
+typedef struct DestroyEntry DestroyEntry;
+struct DestroyEntry
+{
+    Entity ent;
+    unsigned int pass;
+};
+union { Pool pool; DestroyEntry *array; } destroyed; /* list of destroyed, will
+                                                        _remove() */
+
+/*
+ * life cycle
+ * ----------
+ *
+ *     doesn't exist (or removed):
+ *         emap[ent] = NULL
+ *         destroyed_map[ent] = NULL
+ *         ent not in destroyed
+ *
+ *         on entity_create() go to exists
+ *
+ *     exists:
+ *         emap[ent] = dummy_ptr
+ *         destroyed_map[ent] = NULL
+ *         ent not in destroyed
+ *
+ *         on entity_destroy() go to destroyed
+ *
+ *     destroyed:
+ *         emap[ent] = NULL
+ *         destroyed_map[ent] = dummy_ptr
+ *         { ent, pass } in destroyed
+ *
+ *         each update if ++pass >= 2 then _remove() (go back to doesn't exist)
+ */
 
 /* ------------------------------------------------------------------------- */
 
-Entity entity_new()
+static inline bool _exists(Entity ent)
 {
-    return top++;
+    return entitymap_get(emap, ent) == dummy_ptr;
+}
+
+Entity entity_create()
+{
+    unsigned int e = 0;
+
+    while (_exists(e))
+        ++e;
+
+    entitymap_set(emap, e, dummy_ptr);
+    printf("new id: %u\n", e);
+    return e;
+}
+
+/* actually remove an entity entirely */
+static inline void _remove(Entity ent)
+{
+    printf("%u removed\n", ent);
+    entitymap_set(emap, ent, NULL);
+    entitymap_set(destroyed_map, ent, NULL);
+}
+
+void entity_destroy(Entity ent)
+{
+    DestroyEntry *entry;
+
+    if (!_exists(ent))
+        return;
+    if (entitymap_get(destroyed_map, ent) == dummy_ptr)
+        return; /* already noted */
+
+    entitymap_set(destroyed_map, ent, dummy_ptr);
+    entry = pool_new_obj(&destroyed.pool);
+    *entry = (DestroyEntry) { ent, 0 };
+}
+
+bool entity_destroyed(Entity ent)
+{
+    return entitymap_get(destroyed_map, ent) == dummy_ptr;
+}
+
+/* ------------------------------------------------------------------------- */
+
+void entity_init()
+{
+    emap = entitymap_new(NULL);
+    destroyed_map = entitymap_new(NULL);
+    pool_init(&destroyed.pool, sizeof(DestroyEntry), NULL);
+}
+void entity_deinit()
+{
+    pool_deinit(&destroyed.pool);
+    entitymap_free(destroyed_map);
+    entitymap_free(emap);
+}
+
+void entity_update_all()
+{
+    /* check destroyeds -- first pass we let go, second pass we remove */
+    for (unsigned int i = 0; i < destroyed.pool.num; ++i)
+        if (destroyed.array[i].pass == 0)
+        {
+            printf("%u first pass\n", destroyed.array[i].ent);
+            ++destroyed.array[i].pass;
+        }
+        else
+        {
+            _remove(destroyed.array[i].ent);
+            pool_free_obj(&destroyed.pool, &destroyed.array[i]);
+        }
 }
 
 void entity_save(Entity *ent, FILE *file)
--- src/entity.h
+++ src/entity.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "script.h"
 
@@ -10,12 +11,17 @@ SCRIPT(entity,
 
         typedef unsigned int Entity;
 
-        EXPORT Entity entity_new();
+        EXPORT Entity entity_create();
+        EXPORT void entity_destroy(Entity ent);
+        EXPORT bool entity_destroyed(Entity ent);
 
       )
 
 /* script_end */
 
+void entity_init();
+void entity_deinit();
+void entity_update_all();
 void entity_save(Entity *ent, FILE *file);  /* save/load just the id */
 void entity_load(Entity *ent, FILE *file);
 
