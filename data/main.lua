0263d667a3393d11f971bee4b45237d00d647274
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-01-07 00:14:16 +0300

    main.lua runs script given as command-line argument

--- data/main.lua
+++ data/main.lua
@@ -4,5 +4,5 @@ package.path = package.path .. ';' .. cgame_data_path .. '/?.lua'
 -- load 'cgame' module as global
 cgame = require 'cgame'
 
-dofile(cgame_data_path .. '/test/test.lua')
+dofile(cgame_data_path .. (cgame_args[1] or '/test/test.lua'))
 
