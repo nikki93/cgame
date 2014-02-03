558e4e551235e2a6ace3ec3a995012564bf01278
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-02-02 20:42:26 -0500

    add gfx system for common graphics routines

---
+++ src/gfx.c
@@ -0,0 +1,70 @@
+#include "gfx.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static GLint _compile_shader(GLuint shader, const char *filename)
+{
+    char *file_contents, log[512];
+    long input_file_size;
+    FILE *input_file;
+    GLint status;
+
+    input_file = fopen(filename, "rb");
+    fseek(input_file, 0, SEEK_END);
+    input_file_size = ftell(input_file);
+    rewind(input_file);
+    file_contents = malloc((input_file_size + 1) * (sizeof(char)));
+    fread(file_contents, sizeof(char), input_file_size, input_file);
+    fclose(input_file);
+    file_contents[input_file_size] = '\0';
+
+    printf("text: compiling shader '%s' ...", filename);
+
+    glShaderSource(shader, 1, (const GLchar **) &file_contents, NULL);
+    glCompileShader(shader);
+
+    free(file_contents);
+
+    /* log */
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    printf(status ? " successful\n" : " unsuccessful\n");
+    glGetShaderInfoLog(shader, 512, NULL, log);
+    printf("%s", log);
+
+    return status;
+}
+
+GLuint gfx_create_program(const char *vert_path,
+                          const char *geom_path,
+                          const char *frag_path)
+{
+    GLuint vert, geom, frag, program;
+
+#define compile(shader, type)                           \
+    if (shader##_path)                                  \
+    {                                                   \
+        shader = glCreateShader(type);                  \
+        if (!_compile_shader(shader, shader##_path))    \
+            return 0;                                   \
+    }
+
+    compile(vert, GL_VERTEX_SHADER);
+    compile(geom, GL_GEOMETRY_SHADER);
+    compile(frag, GL_FRAGMENT_SHADER);
+
+    program = glCreateProgram();
+
+    if (vert_path) glAttachShader(program, vert);
+    if (geom_path) glAttachShader(program, geom);
+    if (frag_path) glAttachShader(program, frag);
+
+    glLinkProgram(program);
+
+    /* GL will automatically detach and free shaders when program is deleted */
+    if (vert_path) glDeleteShader(vert);
+    if (geom_path) glDeleteShader(geom);
+    if (frag_path) glDeleteShader(frag);
+
+    return program;
+}
---
+++ src/gfx.h
@@ -0,0 +1,37 @@
+#ifndef GFX_H
+#define GFX_H
+
+#include <GL/glew.h>
+
+/*
+ * compile, link program given paths to shader files, possibly NULL,
+ * doesn't glUseProgram(...)
+ */
+GLuint gfx_create_program(const char *vert_path,
+                          const char *geom_path,
+                          const char *frag_path);
+void gfx_free_program(GLuint program);
+
+/* get pointer offset of 'field' in struct 'type' */
+#define poffsetof(type, field)                  \
+    ((void *) (&((type *) 0)->field))
+
+/*
+ * bind vertex attribute data to 'field' in struct 'type' -- gl_type
+ * is the type of the parameter (GL_FLOAT etc.), components is the
+ * number of components, param_name is a string containing the name of
+ * the parameter as it appears in the program
+ */
+#define gfx_bind_vertex_attrib(program, gl_type, components,            \
+                               param_name, type, field)                 \
+    do                                                                  \
+    {                                                                   \
+        GLuint a__ = glGetAttribLocation(program, param_name);          \
+        glVertexAttribPointer(a__, components, gl_type, GL_FALSE,       \
+                              sizeof(type),                             \
+                              poffsetof(type, field));                  \
+        glEnableVertexAttribArray(a__);                                 \
+    } while (0)
+
+
+#endif
