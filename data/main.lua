49c3c39f443fe708159b08fddae73db6c61f8dbb
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-02-22 03:37:28 -0500

    run /usr/scratch.lua on shift+R

--- data/main.lua
+++ data/main.lua
@@ -5,7 +5,19 @@ package.path = package.path .. ';' .. cgame_data_path .. '/?.lua'
 cgame = require 'cgame'
 
 -- print to console
-print = cgame.console_puts
+print = function (k) cgame.console_puts(tostring(k)) end
 
-dofile(cgame_data_path .. (cgame_args[1] or '/test/test.lua'))
+-- run scratch buffer on shift+R
+cgame.add_system
+{
+    name = 'scratch',
+
+    key_down = function (key)
+        if key == cgame.KC_R and cgame.input_key_down(cgame.KC_LEFT_SHIFT) then
+            dofile(cgame_usr_path .. 'scratch.lua')
+        end
+    end,
+}
+
+dofile(cgame_data_path .. (cgame_args[1] or 'test/test.lua'))
 
