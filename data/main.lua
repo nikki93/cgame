6c01dcb852d423adabb3bb3a0267ad29da83ff8f
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-07 00:54:01 -0500

    add some aliases in Lua to reduce typing

--- data/main.lua
+++ data/main.lua
@@ -3,11 +3,15 @@ package.path = package.path .. ';' .. cgame_data_path .. '/?.lua'
 
 -- load 'cgame' module as global
 cgame = require 'cgame'
+
+-- some aliases
 cg = cgame
+cs = cgame.systems
+cg.args = cgame_args
 
 -- print to console
-print = function (k) cgame.console_puts(tostring(k)) end
+print = function (k) cs.console.puts(tostring(k)) end
 
 -- got startup script argument?
-if cgame_args[1] then dofile(cgame_args[1]) end
+if cg.args[1] then dofile(cg.args[1]) end
 
