8b3a37309ca75accac5b16bbe54f76377263b88d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-07 00:54:40 -0500

    update test.lua, test_huge.lua to use new cgame.system interface

--- data/test/test.lua
+++ data/test/test.lua
@@ -1,20 +1,13 @@
 require 'test.oscillator'
 require 'test.rotator'
 
-GRID_SIZE = 32.0
-WIN_WIDTH = 800
-WIN_HEIGHT = 600
+-- add camera: 32 pixels is one unit
 
--- add camera
+camera = cs.entity.create()
 
-camera = cgame.entity_create()
-
-cgame.transform_add(camera)
-cgame.camera_add(camera)
-cgame.camera_set_viewport_size(
-    camera,
-    cgame.vec2(WIN_WIDTH / GRID_SIZE, WIN_HEIGHT / GRID_SIZE)
-)
+cs.transform.add(camera)
+cs.camera.add(camera)
+cs.camera.set_viewport_size(camera, cs.game.get_window_size() / 32.0)
 
 -- add some blocks
 
@@ -26,79 +19,70 @@ end
 
 local n_blocks = 20
 for i = 0, n_blocks do
-    local block = cgame.entity_create()
+    local block = cs.entity.create()
 
-    local pos = cgame.vec2(i - 8, i - 8)
+    local pos = cg.vec2(i - 8, i - 8)
 
-    cgame.transform_add(block)
-    cgame.transform_set_position(block, pos)
+    cs.transform.add(block)
+    cs.transform.set_position(block, pos)
 
-    cgame.sprite_add(block)
+    cs.sprite.add(block)
     if i / 2 == math.floor(i / 2) then
-        cgame.sprite_set_cell(block, cgame.vec2( 0.0, 32.0))
+        cs.sprite.set_cell(block, cg.vec2( 0.0, 32.0))
     else
-        cgame.sprite_set_cell(block, cgame.vec2(32.0, 32.0))
+        cs.sprite.set_cell(block, cg.vec2(32.0, 32.0))
     end
-    cgame.sprite_set_size(block, cgame.vec2(32.0, 32.0))
+    cs.sprite.set_size(block, cg.vec2(32.0, 32.0))
 
-    oscillator_set(block, { amp = 1, freq = 1, phase = 5 * i / n_blocks })
-    rotator_set(block, 2 * math.pi)
+    cs.oscillator.add(block, { amp = 1, freq = 1, phase = 5 * i / n_blocks })
+    cs.rotator.add(block, 2 * math.pi)
 end
 
 -- add player
 
-player = cgame.add
+player = cg.add
 {
     transform = {
-        position = cgame.vec2(0, 0),
-        scale = cgame.vec2(2, 2),
+        position = cg.vec2(0, 0),
+        scale = cg.vec2(2, 2),
         rotation = math.pi / 16,
     },
-    sprite = { cell = cgame.vec2(0, 32), size = cgame.vec2(32, 32) },
+    sprite = { cell = cg.vec2(0, 32), size = cg.vec2(32, 32) },
 }
 
-rchild = cgame.add
+rchild = cg.add
 {
     transform = {
         parent = player,
-        position = cgame.vec2(1, 0),
-        scale = cgame.vec2(0.5, 0.5),
+        position = cg.vec2(1, 0),
+        scale = cg.vec2(0.5, 0.5),
     },
-    sprite = { cell = cgame.vec2(32, 32), size = cgame.vec2(32, 32) },
+    sprite = { cell = cg.vec2(32, 32), size = cg.vec2(32, 32) },
 }
 
-lchild = cgame.add
+lchild = cg.add
 {
     transform = {
         parent = player,
-        position = cgame.vec2(-1, 0),
-        scale = cgame.vec2(0.5, 0.5),
+        position = cg.vec2(-1, 0),
+        scale = cg.vec2(0.5, 0.5),
     },
-    sprite = { cell = cgame.vec2(32, 32), size = cgame.vec2(32, 32) },
+    sprite = { cell = cg.vec2(32, 32), size = cg.vec2(32, 32) },
 }
 
 -- who gets keyboard?
 
---cgame.keyboard_controlled_add(camera)
-cgame.keyboard_controlled_add(player)
+--cs.keyboard.controlled_add(camera)
+cs.keyboard.controlled_add(player)
 
 -- entity destruction
 
-cgame.system_add
-{
-    name = 'destroyer',
-
-    update_all = function ()
-        for i = 1, 9 do
-            if cgame.input_key_down('KC_' .. i) then
-                cgame.entity_destroy(i)
-            end
-        end
-
-        if cgame.input_mouse_down(cgame.MC_LEFT) then
-            p = cgame.input_get_mouse_pos_unit()
-            p = cgame.transform_local_to_world(camera, p)
-            print(p.x .. ', ' .. p.y)
+-- for each key 1 .. 9 pressed, destroy that entity
+cs.destroyer = {}
+function cs.destroyer.update_all()
+    for i = 1, 9 do
+        if cs.input.key_down('KC_' .. i) then
+            cs.entity.destroy(cg.Entity { id = i })
         end
     end
-}
+end
--- data/test/test_huge.lua
+++ data/test/test_huge.lua
@@ -1,20 +1,12 @@
 require 'test.oscillator'
 require 'test.rotator'
 
-GRID_SIZE = 32.0
-WIN_WIDTH = 800
-WIN_HEIGHT = 600
+-- add camera: 32 pixels is one unit
 
--- add camera
-
-local camera = cgame.entity_create()
-
-cgame.transform_add(camera)
-cgame.camera_add(camera)
-cgame.camera_set_viewport_size(
-    camera,
-    cgame.vec2(WIN_WIDTH / GRID_SIZE, WIN_HEIGHT / GRID_SIZE)
-)
+local camera = cs.entity.create()
+cs.transform.add(camera)
+cs.camera.add(camera)
+cs.camera.set_viewport_size(camera, cs.game.get_window_size() / 32.0)
 
 -- add some blocks
 
@@ -24,47 +16,47 @@ function symrand()
     return 2 * math.random() - 1
 end
 
-local n_blocks = cgame_args[2] or 30000
+local n_blocks = cg.args[2] or 30000
 print('creating ' .. n_blocks .. ' blocks')
 for i = 0, n_blocks do
-    local block = cgame.entity_create()
+    local block = cs.entity.create()
 
     local y = 8 * symrand()
     while math.abs(y) < 1.5 do
         y = 8 * symrand()
     end
-    local pos = cgame.vec2(8 * symrand(), y)
+    local pos = cg.vec2(8 * symrand(), y)
 
-    cgame.transform_add(block)
-    cgame.transform_set_position(block, pos)
+    cs.transform.add(block)
+    cs.transform.set_position(block, pos)
 
-    cgame.sprite_add(block)
+    cs.sprite.add(block)
     if symrand() < 0 then
-        cgame.sprite_set_cell(block, cgame.vec2( 0.0, 32.0))
+        cs.sprite.set_cell(block, cg.vec2( 0.0, 32.0))
     else
-        cgame.sprite_set_cell(block, cgame.vec2(32.0, 32.0))
+        cs.sprite.set_cell(block, cg.vec2(32.0, 32.0))
     end
-    cgame.sprite_set_size(block, cgame.vec2(32.0, 32.0))
+    cs.sprite.set_size(block, cg.vec2(32.0, 32.0))
 
-    oscillator_set(block, { amp = 3 * math.random(), freq = math.random() })
-    rotator_set(block, math.random() * math.pi)
+    cs.oscillator.add(block, { amp = 3 * math.random(), freq = math.random() })
+    cs.rotator.add(block, math.random() * math.pi)
 end
 
 -- add player
 
-local player = cgame.entity_create()
+local player = cs.entity.create()
 
-cgame.transform_add(player)
-cgame.transform_set_position(player, cgame.vec2(0.0, 0.0))
+cs.transform.add(player)
+cs.transform.set_position(player, cg.vec2(0.0, 0.0))
 
-cgame.sprite_add(player)
-cgame.sprite_set_cell(player, cgame.vec2( 0.0, 32.0))
-cgame.sprite_set_size(player, cgame.vec2(32.0, 32.0))
+cs.sprite.add(player)
+cs.sprite.set_cell(player, cg.vec2( 0.0, 32.0))
+cs.sprite.set_size(player, cg.vec2(32.0, 32.0))
 
-cgame.transform_set_scale(player, cgame.vec2(2, 2))
+cs.transform.set_scale(player, cg.vec2(2, 2))
 
 -- who gets keyboard?
 
-cgame.keyboard_controlled_add(camera)
---cgame.keyboard_controlled_add(player)
+cs.keyboard.controlled_add(camera)
+--cs.keyboard.controlled_add(player)
 
