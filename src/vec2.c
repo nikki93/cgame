0284b351ab7686615bbb7b8997db7ebcb0699428
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-19 18:51:38 -0400

    add vec2_dist(...)

--- src/vec2.c
+++ src/vec2.c
@@ -39,6 +39,10 @@ Vec2 vec2_normalize(Vec2 v)
         return v;
     return vec2_scalar_div(v, vec2_len(v));
 }
+Scalar vec2_dist(Vec2 u, Vec2 v)
+{
+    return vec2_len(vec2_sub(u, v));
+}
 
 Vec2 vec2_rot(Vec2 v, Scalar rot)
 {
