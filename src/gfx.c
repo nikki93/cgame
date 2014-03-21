645e1f7ad0b3673dbbacdf4c22a070e992874b1d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-20 22:35:20 -0400

    cleanup

--- src/gfx.c
+++ src/gfx.c
@@ -19,7 +19,7 @@ static GLint _compile_shader(GLuint shader, const char *filename)
     fclose(input_file);
     file_contents[input_file_size] = '\0';
 
-    printf("text: compiling shader '%s' ...", filename);
+    printf("gfx: compiling shader '%s' ...", filename);
 
     glShaderSource(shader, 1, (const GLchar **) &file_contents, NULL);
     glCompileShader(shader);
