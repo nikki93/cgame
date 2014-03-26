57a44cd25f510b52afe8b5055cab9090b9a0fe70
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-26 14:41:16 -0400

    add edit text, fps text, console text to 'builtin' group

--- data/script/cgame/fps.lua
+++ data/script/cgame/fps.lua
@@ -7,6 +7,7 @@ local timer = 0;
 -- display gui entity
 local display = cg.add {
     entity = { persistent = true },
+    group = { groups = 'builtin' },
     gui_text = { str = 'fps: ...' },
     gui = { color = cg.color(0, 0.4, 0.1, 1) },
 }
