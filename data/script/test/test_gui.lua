1df2ebf184f21fd401ea9a209873ec10ab44f141
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-27 22:43:16 -0400

    nicer gui test

--- data/script/test/test_gui.lua
+++ data/script/test/test_gui.lua
@@ -1,35 +1,90 @@
-win = cg.add {
-    transform = { position = cg.vec2(400, -100) },
-    gui_rect = { size = cg.vec2(200, 400) },
-    gui = {color = cg.color(0.2, 0.2, 0.9, 0.5) },
-}
-text = cg.add {
-    transform = { parent = win },
-    gui = {color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
-    gui_text = { str = 'test line 1' },
-}
-text2 = cg.add {
-    transform = { parent = win },
-    gui = {color = cg.color_red, valign = cg.GA_TABLE, halign = cg.GA_MIN },
-    gui_text = { str = 'test line 2...' },
+win1 = cg.add {
+    gui_rect = {},
+    gui = {
+        color = cg.color(0.2, 0.2, 0.9, 0.5),
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MAX,
+    },
 }
 
 win2 = cg.add {
-    transform = { position = cg.vec2(20, -300), parent = win },
-    gui_rect = { size = cg.vec2(150, 150) },
+    gui_rect = {},
+    gui = {
+        color = cg.color(0.2, 0.2, 0.9, 0.5),
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MAX,
+    },
+}
+
+text1 = cg.add {
+    transform = { parent = win1 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'transform' },
+}
+
+text2 = cg.add {
+    transform = { parent = win2 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'sprite' },
+}
+
+win3 = cg.add {
+    transform = { parent = win2 },
+    gui_rect = {},
     gui = {
         color = cg.color(0.2, 0.9, 0.2, 0.5),
         valign = cg.GA_TABLE,
+        halign = cg.GA_MIN,
     },
 }
 text3 = cg.add {
-    transform = { parent = win2 },
-    gui = {color = cg.color_red, valign = cg.GA_TABLE, halign = cg.GA_MIN },
-    gui_text = { str = 'test line 2...' },
+    transform = { parent = win3 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'size: (?, ?)' },
 }
 
+win4 = cg.add {
+    transform = { parent = win2 },
+    gui_rect = {},
+    gui = {
+        color = cg.color(0.2, 0.9, 0.2, 0.5),
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN,
+    },
+}
+
+win5 = cg.add {
+    transform = { parent = win4 },
+    gui_rect = {},
+    gui = {
+        color = cg.color(0.9, 0.5, 0.2, 0.5),
+        valign = cg.GA_MAX,
+        halign = cg.GA_TABLE,
+    },
+}
 text4 = cg.add {
-    transform = { parent = win },
-    gui = {color = cg.color_red, valign = cg.GA_TABLE, halign = cg.GA_MIN },
-    gui_text = { str = 'test line 2...' },
+    transform = { parent = win5 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'prop1' },
+}
+text4 = cg.add {
+    transform = { parent = win5 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'prop2' },
+}
+
+
+win6 = cg.add {
+    transform = { parent = win4 },
+    gui_rect = {},
+    gui = {
+        color = cg.color(0.9, 0.5, 0.2, 0.5),
+        valign = cg.GA_MAX,
+        halign = cg.GA_TABLE,
+    },
+}
+text5 = cg.add {
+    transform = { parent = win6 },
+    gui = { color = cg.color_white, valign = cg.GA_TABLE, halign = cg.GA_MIN },
+    gui_text = { str = 'prop3' },
 }
