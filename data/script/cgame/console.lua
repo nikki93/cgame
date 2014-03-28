03752867dc2ccdf20d663168f4dfc9f8ce754781
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-03-28 15:29:15 -0400

    console print now takes any number of arguments

--- data/script/cgame/console.lua
+++ data/script/cgame/console.lua
@@ -11,4 +11,11 @@ local console_text = cg.add {
 cs.console.set_entity(console_text)
 
 -- rebind Lua 'print' to print to console
-print = function (k) cs.console.puts(tostring(k)) end
+function print(...)
+    local args = table.pack(...)
+    local sargs = {}
+    for i = 1, args.n do
+        sargs[i] = tostring(args[i])
+    end
+    cs.console.puts(table.concat(sargs, '    '))
+end
