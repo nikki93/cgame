9e8aec1e7b386267a1cd46b9de46e6e55557e3bb
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-04-09 02:17:36 -0400

    more windows in test_gui.lua

--- data/script/test/test_gui.lua
+++ data/script/test/test_gui.lua
@@ -35,3 +35,66 @@ text2 = cg.add {
     },
     gui_text = { str = 'prop1: x, y, z' },
 }
+
+win = cg.add {
+    gui_window = { title = 'parent window 2!', closeable = false },
+    gui = {
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MAX,
+    },
+}
+
+win2 = cg.add {
+    gui_window = { title = 'child window!' },
+    gui_rect = { hfill = true },
+    transform = { parent = cs.gui_window.get_body(win) },
+    gui = {
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN,
+    },
+}
+
+text2 = cg.add {
+    transform = { parent = cs.gui_window.get_body(win2) },
+    gui = {
+        color = cg.color_white,
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN
+    },
+    gui_text = { str = 'body is a very very long text\nof many lines!' },
+}
+
+win = cg.add {
+    transform = { position = cg.vec2(42, -42) },
+    gui_window = { title = 'free window!' },
+}
+
+text1 = cg.add {
+    transform = { parent = cs.gui_window.get_body(win) },
+    gui = {
+        color = cg.color_white,
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN
+    },
+    gui_text = { str = 'drag titlebar to move' },
+}
+
+win2 = cg.add {
+    gui_window = { title = 'child window!' },
+    gui_rect = { hfill = true },
+    transform = { parent = cs.gui_window.get_body(win) },
+    gui = {
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN,
+    },
+}
+
+text2 = cg.add {
+    transform = { parent = cs.gui_window.get_body(win2) },
+    gui = {
+        color = cg.color_white,
+        valign = cg.GA_TABLE,
+        halign = cg.GA_MIN
+    },
+    gui_text = { str = 'I move with my parent!' },
+}
