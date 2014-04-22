e622cff9f468b6bf56b64c44a186227bb90b0367
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-04-22 14:16:09 -0400

    wrap add poly functions to have default radius of zero

--- data/script/cgame/physics.lua
+++ data/script/cgame/physics.lua
@@ -1,9 +1,16 @@
 local ffi = require 'ffi'
 
+local old_physics_shape_add_box = cg.physics_shape_add_box
+function cg.physics_shape_add_box(ent, b, r)
+    r = r or 0
+    return old_physics_shape_add_box(ent, b, r)
+end
+
 local old_physics_shape_add_poly = cg.physics_shape_add_poly
-function cg.physics_shape_add_poly(ent, verts)
+function cg.physics_shape_add_poly(ent, verts, r)
+    r = r or 0
     local n = #verts
-    return old_physics_shape_add_poly(ent, n, ffi.new('Vec2[?]', n, verts))
+    return old_physics_shape_add_poly(ent, n, ffi.new('Vec2[?]', n, verts), r)
 end
 
 local old_physics_convex_hull = cg.physics_convex_hull
