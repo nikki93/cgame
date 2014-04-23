549f7cb5e94b0d3d35063eb57a63475bcd98e2ce
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-04-23 00:48:39 -0400

    remove s/l binds in main.lua

--- data/script/main.lua
+++ data/script/main.lua
@@ -24,20 +24,6 @@ function cs.app.key_down(key)
         print("destroying group 'default'")
         cs.group.destroy('default')
 
-    elseif key == cg.KC_S then
-        print("saving group 'default' to file '"
-                  .. cgame_usr_path .. "test.sav'")
-        cs.group.set_save_filter('default', true)
-        local s = cs.serializer.open_file(cgame_usr_path .. 'test.sav')
-        cs.system.save_all(s)
-        cs.serializer.close(s)
-
-    elseif key == cg.KC_L then
-        print("loading from file '" .. cgame_usr_path .. "test.sav'")
-        local d = cs.deserializer.open_file(cgame_usr_path .. 'test.sav')
-        cs.system.load_all(d)
-        cs.deserializer.close(d)
-
     end
 end
 
