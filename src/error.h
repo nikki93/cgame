03fabc3cca8c322a09bb7aeb0577af0574172d35
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-05-01 02:40:10 -0400

    allow no format string in asserts, or no format parameters in error

--- src/error.h
+++ src/error.h
@@ -9,13 +9,21 @@
 /* printf-style error formatting */
 void errorf(const char *fmt, ...);
 
+/* evaluates to a 'filename:linenumber: ' string */
+#define line_str__(line)                        \
+    __FILE__ ":" #line ": "
+#define line_str_(line)                         \
+    line_str__(line)
+#define line_str()                              \
+    line_str_(__LINE__)
+
 /* printf-style error formatting with file name, line number */
-#define error(fmt, ...)                                 \
-    errorf(__FILE__ ":%d: " fmt, __LINE__, __VA_ARGS__)
+#define error(...)                              \
+    errorf(line_str() __VA_ARGS__)
 
 /* printf-style error formatting for an assertion, also prints condition */
-#define error_assert(cond, fmt, ...)                            \
-    error("assertion '" #cond "' failed: " fmt, __VA_ARGS__)
+#define error_assert(cond, ...)                         \
+    error("assertion '" #cond "' failed ... " __VA_ARGS__)
 
 #endif
     
