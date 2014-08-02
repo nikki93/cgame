ac6886505b54d285e15d8f1c8736be4db7afd50d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-08-02 01:46:54 -0700

    make the sound system be one sound per entity

--- src/sound.c
+++ src/sound.c
@@ -11,129 +11,35 @@
 #include "array.h"
 #include "error.h"
 
-typedef struct SoundSource SoundSource;
-struct SoundSource
-{
-    EntityPoolElem pool_elem;
-
-    Array *sounds;
-};
-
 typedef struct Sound Sound;
 struct Sound
 {
-    char *name;
+    EntityPoolElem pool_elem;
+
     char *path;
     ga_Handle *handle;
-};
-
-typedef struct SoundCacheEntry SoundCacheEntry;
-struct SoundCacheEntry
-{
-    char *path;
-    ga_Sound *sound;
+    bool finish_destroy;
 };
 
 static EntityPool *pool;
-
-static Array *cache_entries;
 
 static gau_Manager *mgr;
 static ga_Mixer *mixer;
 static ga_StreamManager *stream_mgr;
 
-void sound_add(Entity ent)
+static void _release(Sound *sound)
 {
-    SoundSource *source;
-
-    if (entitypool_get(pool, ent))
-        return;
-
-    source = entitypool_add(pool, ent);
-    source->sounds = array_new(Sound);
-}
-
-/* release resources, but don't remove from array */
-static void _release_sound(Sound *sound)
-{
-    free(sound->name);
+    /* path */
     free(sound->path);
+    sound->path = NULL;
+
+    /* handle */
     if (sound->handle)
         ga_handle_destroy(sound->handle);
-}
-
-/* release resources, but don't remove from pool */
-static void _release_source(SoundSource *source)
-{
-    Sound *sound;
-
-    array_foreach(sound, source->sounds)
-        _release_sound(sound);
-    array_free(source->sounds);
-}
-
-void sound_remove(Entity ent)
-{
-    SoundSource *source;
-
-    source = entitypool_get(pool, ent);
-    if (!source)
-        return;
-
-    _release_source(source);
-    entitypool_remove(pool, ent);
-}
-
-bool sound_has(Entity ent)
-{
-    return entitypool_get(pool, ent) != NULL;
-}
-
-static Sound *_source_get_sound(SoundSource *source, const char *name)
-{
-    Sound *sound;
-    array_foreach(sound, source->sounds)
-        if (!strcmp(sound->name, name))
-            return sound;
-    error("no sound with name '%s' for this entity", name);
-}
-
-static SoundSource *_get_source(Entity ent)
-{
-    SoundSource *source = entitypool_get(pool, ent);
-    error_assert(source, "entity must be in sound system");
-    return source;
-}
-
-static Sound *_get_sound(Entity ent, const char *name)
-{
-    return _source_get_sound(_get_source(ent), name);
-}
-
-void sound_add_sound(Entity ent, const char *name)
-{
-    SoundSource *source;
-    Sound *sound;
-
-    source = _get_source(ent);
-    sound = array_add(source->sounds);
-    sound->name = malloc(strlen(name) + 1);
-    strcpy(sound->name, name);
-    sound->path = NULL;
     sound->handle = NULL;
 }
-void sound_remove_sound(Entity ent, const char *name)
-{
-    SoundSource *source;
-    Sound *sound;
 
-    source = _get_source(ent);
-    sound = _source_get_sound(source, name);
-    _release_sound(sound);
-    array_quick_remove(source->sounds,
-                       sound - (Sound *) array_begin(source->sounds));
-}
-
+/* figure out format from path -- uses extension */
 static const char *_format(const char *path)
 {
     const char *dot = NULL, *c;
@@ -145,42 +51,173 @@ static const char *_format(const char *path)
     error("unknown sound format for file '%s'", path);
 }
 
-void sound_set_path(Entity ent, const char *name, const char *path)
+static void _set_path(Sound *sound, const char *path)
 {
-    Sound *sound = _get_sound(ent, name);
+    const char *format;
+    bool prev_playing;
+    ga_Sound *src;
 
-    free(sound->path);
+    prev_playing = sound->handle && ga_handle_playing(sound->handle);
+    _release(sound);
+
+    /* copy path */
     sound->path = malloc(strlen(path) + 1);
     strcpy(sound->path, path);
 
-    if (sound->handle)
-        ga_handle_destroy(sound->handle);
-    sound->handle = gau_create_handle_buffered_file(
-        mixer, stream_mgr, path, _format(path),
-        NULL, NULL, NULL
-        );
-}
-const char *sound_get_path(Entity ent, const char *name)
-{
-    return _get_sound(ent, name)->path;
-}
+    /* create handle */
+    format = _format(path);
+    if (!strcmp(format, "ogg"))
+    {
+        sound->handle = gau_create_handle_buffered_file(
+            mixer, stream_mgr, path, _format(path),
+            NULL, NULL, NULL
+            );
+    }
+    else
+    {
+        src = gau_load_sound_file(path, format);
+        if (src)
+            sound->handle = gau_create_handle_sound(
+                mixer, src,
+                NULL, NULL, NULL
+                );
+    }
+    if (!sound->handle)
+    {
+        _release(sound);
+        error("couldn't load sound from path '%s', check path and format",
+              path);
+    }
 
-void sound_play(Entity ent, const char *name)
-{
-    Sound *sound = _get_sound(ent, name);
-    if (sound->handle)
+    if (prev_playing)
         ga_handle_play(sound->handle);
 }
-void sound_stop(Entity ent, const char *name)
+
+void sound_add(Entity ent)
 {
-    Sound *sound = _get_sound(ent, name);
-    if (sound->handle)
+    Sound *sound;
+
+    if (entitypool_get(pool, ent))
+        return;
+
+    sound = entitypool_add(pool, ent);
+    sound->path = NULL;
+    sound->handle = NULL;
+    sound->finish_destroy = true;
+}
+
+void sound_remove(Entity ent)
+{
+    Sound *sound;
+
+    sound = entitypool_get(pool, ent);
+    if (!sound)
+        return;
+
+    sound = entitypool_get(pool, ent);
+    _release(sound);
+    entitypool_remove(pool, ent);
+}
+
+bool sound_has(Entity ent)
+{
+    return entitypool_get(pool, ent) != NULL;
+}
+
+void sound_set_path(Entity ent, const char *path)
+{
+    Sound *sound;
+    const char *format;
+    bool prev_playing;
+    ga_Sound *src;
+
+    sound =  entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+
+    /* check if currently playing, release */
+    prev_playing = sound->handle && ga_handle_playing(sound->handle);
+    _release(sound);
+
+    /* copy path */
+    sound->path = malloc(strlen(path) + 1);
+    strcpy(sound->path, path);
+
+    /* create handle */
+    format = _format(path);
+    if (!strcmp(format, "ogg"))
+    {
+        sound->handle = gau_create_handle_buffered_file(
+            mixer, stream_mgr, path, _format(path),
+            NULL, NULL, NULL
+            );
+    }
+    else
+    {
+        src = gau_load_sound_file(path, format);
+        if (src)
+            sound->handle = gau_create_handle_sound(
+                mixer, src,
+                NULL, NULL, NULL
+                );
+    }
+    if (!sound->handle)
+    {
+        _release(sound);
+        error("couldn't load sound from path '%s', check path and format",
+              path);
+    }
+
+    /* play new sound if old one was playing */
+    if (prev_playing)
+        ga_handle_play(sound->handle);
+}
+const char *sound_get_path(Entity ent)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    return sound->path;
+}
+
+void sound_set_playing(Entity ent, bool playing)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    if (playing)
+        ga_handle_play(sound->handle);
+    else
         ga_handle_stop(sound->handle);
 }
-bool sound_playing(Entity ent, const char *name)
+bool sound_get_playing(Entity ent)
 {
-    Sound *sound = _get_sound(ent, name);
-    return sound->handle && ga_handle_playing(sound->handle);
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    return ga_handle_playing(sound->handle);
+}
+
+void sound_set_seek(Entity ent, int seek)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    ga_handle_seek(sound->handle, seek);
+}
+int sound_get_seek(Entity ent)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    return ga_handle_tell(sound->handle, GA_TELL_PARAM_CURRENT);
+}
+
+void sound_set_finish_destroy(Entity ent, bool finish_destroy)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    sound->finish_destroy = finish_destroy;
+}
+bool sound_get_finish_destroy(Entity ent)
+{
+    Sound *sound = entitypool_get(pool, ent);
+    error_assert(sound, "entity must be in sound system");
+    return sound->finish_destroy;
 }
 
 void sound_init()
@@ -191,14 +228,14 @@ void sound_init()
     mixer = gau_manager_mixer(mgr);
     stream_mgr = gau_manager_streamManager(mgr);
 
-    pool = entitypool_new(SoundSource);
+    pool = entitypool_new(Sound);
 }
 void sound_deinit()
 {
-    SoundSource *source;
+    Sound *sound;
 
-    entitypool_foreach(source, pool)
-        _release_source(source);
+    entitypool_foreach(sound, pool)
+        _release(sound);
     entitypool_free(pool);
 
     gau_manager_destroy(mgr);
@@ -207,6 +244,14 @@ void sound_deinit()
 
 void sound_update_all()
 {
+    Sound *sound;
+
+    /* destroy finished sounds that have finish_destroy set */
+    entitypool_foreach(sound, pool)
+        if (sound->finish_destroy
+            && sound->handle && ga_handle_finished(sound->handle))
+            entity_destroy(sound->pool_elem.ent);
+
     entitypool_remove_destroyed(pool, sound_remove);
 
     gau_manager_update(mgr);
--- src/sound.h
+++ src/sound.h
@@ -10,15 +10,17 @@ SCRIPT(sound,
        EXPORT void sound_remove(Entity ent);
        EXPORT bool sound_has(Entity ent);
 
-       EXPORT void sound_add_sound(Entity ent, const char *name);
-       EXPORT void sound_remove_sound(Entity ent, const char *name);
+       EXPORT void sound_set_path(Entity ent, const char *path);
+       EXPORT const char *sound_get_path(Entity ent);
 
-       EXPORT void sound_set_path(Entity ent, const char *name, const char *path);
-       EXPORT const char *sound_get_path(Entity ent, const char *name);
+       EXPORT void sound_set_playing(Entity ent, bool playing);
+       EXPORT bool sound_get_playing(Entity ent);
 
-       EXPORT void sound_play(Entity ent, const char *name);
-       EXPORT void sound_stop(Entity ent, const char *name);
-       EXPORT bool sound_playing(Entity ent, const char *name);
+       EXPORT void sound_set_seek(Entity ent, int seek);
+       EXPORT int sound_get_seek(Entity ent);
+
+       EXPORT void sound_set_finish_destroy(Entity ent, bool finish_destroy);
+       EXPORT bool sound_get_finish_Destroy(Entity ent);
 
     )
 
