a26f2a74de77ad1970664740dfb0dcc8be3d9bc8
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-08-19 00:30:47 -0700

    add one-way platforms to platformer example

--- games/platformer/main.lua
+++ games/platformer/main.lua
@@ -39,15 +39,35 @@ function cs.player.unpaused_update(obj)
     -- gravity
     obj.v.y = obj.v.y - obj.gravity * cs.timing.dt
 
+    -- move filtering
+    local p = cs.transform.get_position(obj.ent)
+    local function filter(e)
+        if cs.oneway.has(e) then
+            local ep = cs.transform.get_position(e)
+            if p.y < ep.y + 0.99 then return false end
+        end
+        return true
+    end
+
     -- move! (first y then x)
     local d = obj.v * cs.timing.dt
-    local cols = cs.bump.slide(obj.ent, cs.transform.get_position(obj.ent) + d)
+    local cols = cs.bump.slide(obj.ent, cs.transform.get_position(obj.ent) + d,
+                               filter)
 
     -- collisions
     for _, col in ipairs(cols) do
         if col.normal.y ~= 0 then obj.v.y = 0 end
         if col.normal.y > 0 then obj.grounded = true end
     end
+end
+
+
+--- oneway ------------------------------------------------------------------
+
+cs.oneway = cg.simple_sys()
+
+function cs.oneway.create(obj)
+    cs.bump.add(obj.ent)
 end
 
 
