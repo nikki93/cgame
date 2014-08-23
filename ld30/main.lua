243c95cb9384d8e1b97db9635e23c2d865aa5aeb
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2014-08-23 07:13:44 -0700

    add portals and world jumping (untested)

--- ld30/main.lua
+++ ld30/main.lua
@@ -4,6 +4,11 @@ function cg.square_random(d)
     return cg.vec2(math.random() * d, math.random() * d)
 end
 
+function file_exists(s)
+    local f = io.open(s, 'r')
+    if f ~= nil then io.close(f) return true else return false end
+end
+
 
 --- cgame config ------------------------------------------------------------
 
@@ -14,13 +19,42 @@ cs.sprite.set_atlas(data_dir .. '/atlas-1.png')
 cs.edit.set_default_file(data_dir .. '/')
 cs.edit.set_default_prefab_file(data_dir .. '/')
 
+
+-----------------------------------------------------------------------------
+
+function save_game(name, groups)
+    cs.group.set_save_filter(groups, true)
+    local s = cg.store_open()
+    cs.system.save_all(s)
+    cg.store_write_file(s, data_dir .. '/' .. name)
+    cg.store_close(s)
+end
+
+function load_game(name)
+    if not file_exists(data_dir .. '/' .. name) then return false end
+    local s = cg.store_open_file(data_dir .. '/' .. name)
+    cs.system.load_all(s)
+    cg.store_close(s)
+    return true
+end
+
+cs.edit.modes.normal['s'] = function ()
+    cs.edit.command_save()
+
+    save_game('portals.lvl', 'portals')
+    print('saved portals!')
+end
+
+
 -----------------------------------------------------------------------------
 
 cs.main = {}
 
+cs.main.world = 'hell'
+
 function cs.main.reload()
-    cs.group.destroy('default')
-    local s = cg.store_open_file(data_dir .. '/1.lvl')
+    cs.group.destroy('world')
+    local s = cg.store_open_file(data_dir .. '/hell.lvl')
     cs.system.load_all(s)
     cg.store_close(s)
 end
@@ -28,6 +62,16 @@ end
 function cs.main.die()
     local player = cs.name.find('player')
     if player then cs.player_control.die(player) end
+end
+
+function cs.main.warp(world)
+    -- save and clear current state
+    save_game(cs.main.world .. '.sav', 'default')
+    cs.groups.destroy('default')
+
+    -- set, load new world
+    cs.main.world = world
+    if not load_game(world .. '.sav') then load_game(world .. '.lvl') end
 end
 
 
@@ -102,7 +146,8 @@ function cs.player_control.unpaused_update(obj)
     d = cg.vec2_normalize(d)
 
     if pressed then 
-        cs.bump.slide(obj.ent, g_player_speed * d * cs.timing.dt)
+        cs.bump.slide(obj.ent, g_player_speed * d * cs.timing.dt,
+                      function (e) return not cs.portal.has(e) end)
     end
 
     if rot then cs.transform.set_rotation(obj.ent, rot) end
@@ -143,11 +188,14 @@ function cs.follower.unpaused_update(obj)
     if player ~= cg.entity_nil then
         local pp = cs.transform.get_position(player)
         local d = pp - cs.transform.get_position(obj.ent)
-        d = cg.vec2_normalize(d)
-        local cols = cs.bump.slide(obj.ent, g_follower_speed * d * cs.timing.dt)
-        for _, col in pairs(cols) do
-            if cs.player_control.has(col.other) then
-                cs.main.die()
+        local len = cg.vec2_len(d)
+        if len < 20 then
+            d = d / len
+            local cols = cs.bump.slide(obj.ent, g_follower_speed * d * cs.timing.dt)
+            for _, col in pairs(cols) do
+                if cs.player_control.has(col.other) then
+                    cs.main.die()
+                end
             end
         end
     end
@@ -230,8 +278,10 @@ cs.camera_follow = cg.simple_sys()
 
 function cs.camera_follow.update_all()
     local cam = cs.camera.get_current_camera()
-    if cam ~= cg.entity_nil and cam ~= cs.edit.camera then
+    if cam ~= cg.entity_nil and cam ~= cs.edit.camera
+    and not cs.camera_follow.has(cam) then
         cs.camera_follow.add(cam)
+        cs.group.set_groups(cam, 'warp')
     end
 
     cs.camera_follow.simple_update_all()
@@ -246,3 +296,31 @@ function cs.camera_follow.unpaused_update(obj)
     end
 end
 
+
+-----------------------------------------------------------------------------
+
+cs.portal = cg.simple_sys()
+
+cg.simple_prop(cs.portal, 'world_1', 'hell')
+cg.simple_prop(cs.portal, 'world_2', 'water')
+
+function cs.portal.create(obj)
+end
+
+function cs.portal.unpaused_update(obj)
+    local cols = cs.bump.sweep(obj.ent)
+    if obj.in_warp == nil then obj.in_warp = false end
+
+    local next_in_warp = false
+    for _, col in ipairs(cols) do
+        if cs.name.get_name(col.other) == 'player' then
+            next_in_warp = true
+            if not obj.in_warp then
+                local ow = (obj.world_1 == cs.main.world)
+                    and obj.world_2 or obj.world_1
+                cs.main.warp(ow)
+            end
+        end
+    end
+    obj.in_warp = next_in_warp
+end
